f92afc6707262f15bcc0f025c3bc7b7aff9e343b
pujiragil <ragilsg164@gmail.com>
2022-04-09 10:10:34 +0700

    test

--- js/custom.js
+++ js/custom.js
@@ -27,25 +27,3 @@ $(".owl-carousel").owlCarousel({
 });
 
 //    end owl carousel script 
-
-
-
-<<<<<<< HEAD
-// /** google_map js **/
-// function myMap() {
-//     var mapProp = {
-//         center: new google.maps.LatLng(40.712775, -74.005973),
-//         zoom: 18,
-//     };
-//     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-// }
-=======
-/** google_map js **/
-// function myMap() {
-//     var mapProp = {
-//         center: new google.maps.LatLng(40.712775, -74.005973),
-//         zoom: 18,
-//     };
-//     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-// }
->>>>>>> b499b8e07addf7712016201b778bd57799fbf90f
